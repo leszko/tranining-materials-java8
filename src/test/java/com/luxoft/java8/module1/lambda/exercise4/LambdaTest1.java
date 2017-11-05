992cc7dd3131e31481e93885de02f2cf075ae437
leszko <rafal.leszko@gmail.com>
2017-11-05 12:37:35 +0100

    Fix exercise4.

--- src/test/java/com/luxoft/java8/module1/lambda/exercise4/LambdaTest1.java
+++ src/test/java/com/luxoft/java8/module1/lambda/exercise4/LambdaTest1.java
@@ -2,10 +2,7 @@ package com.luxoft.java8.module1.lambda.exercise4;
 
 import java.util.function.Function;
 
-/**
- * Rewrite this class to use method reference instead of the lambda expression.
- */
-public class LambdaTest {
+public class LambdaTest1 {
 
   public String process(String s, Function<String, String> f) {
     return f.apply(s);
@@ -16,8 +13,8 @@ public class LambdaTest {
   }
 
   public static void main(String[] args) {
-    LambdaTest lambdaTest = new LambdaTest();
-    String string = lambdaTest.process("hello", s -> lambdaTest.addExclam(s));
+    LambdaTest1 lambdaTest = new LambdaTest1();
+    String string = lambdaTest.process("hello", lambdaTest::addExclam);
     System.out.println(string);
   }
 }
